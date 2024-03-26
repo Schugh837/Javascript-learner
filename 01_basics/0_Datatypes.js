55023ce1abab92b8a6ad1b2f853edeca12888c9d
Schugh837 <87005583+Schugh837@users.noreply.github.com>
2024-03-26 06:30:40 +0000

    datatypes

---
+++ 01_basics/0_Datatypes.js
@@ -0,0 +1,19 @@
+"Use strict"; //Treat all js code as new version
+
+let name = "codeBrew" //string
+let age = 3 //numeric
+let isLoggedIn = "True" //boolean 
+
+//Number
+//bigInt
+//String
+//boolean
+//null:-standalone value
+//undefined
+//symbol==> uniqueness
+//object
+
+console.log(typeof "codeBrew");
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); //object
