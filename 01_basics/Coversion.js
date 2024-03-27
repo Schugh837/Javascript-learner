4fb9f8dd0d8df016a72c1875c91f63f880559725
Schugh837 <87005583+Schugh837@users.noreply.github.com>
2024-03-27 07:40:05 +0000

    coversion done

--- 01_basics/Coversion.js
+++ 01_basics/Coversion.js
@@ -7,7 +7,18 @@
 //let valueInNumber = Number(score)
 //if we convert 33 into number easily converted
 
-let isLoggedIn = 1 
+/*let isLoggedIn = 1 
 
 let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn);
+console.log(booleanIsLoggedIn); */
+
+//********************************************Opreations****************************
+
+let value = 7
+let negValue = -value
+//console.log(negValue);
+
+let str1 = "hello"
+let str2 = " Silky"
+let str3 = str1 + str2
+//console.log(str3);
