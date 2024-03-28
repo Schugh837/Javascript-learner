ad2cebc36c4f5ad9a271660daba728a9da2c73c5
Schugh837 <87005583+Schugh837@users.noreply.github.com>
2024-03-28 11:35:07 +0000

    datatypes

---
+++ 01_basics/datatypes_summry.js
@@ -0,0 +1,14 @@
+// primitive 
+
+// 7 types : string , number, boolean,null,undefined,symbol,BigInt
+
+//Non-primitive 
+
+//Refrnce :- Array,object,functions
+
+
+// function(){}
+
+const myFunction = function(){
+    console.log("hello world");
+}
