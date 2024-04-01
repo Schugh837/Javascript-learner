2f0e697b21e7c8405c15e265716ff96012c2d3ce
Schugh837 <87005583+Schugh837@users.noreply.github.com>
2024-04-01 07:57:22 +0000

    changes done

--- 01_basics/Java.js
+++ 01_basics/Java.js
@@ -1,4 +1,4 @@
-let a = 4 //let is as a var, if we can declare var use let , const
+/* let a = 4 //let is as a var, if we can declare var use let , const
 console.log(a)
 console.log(typeof(a))
 
@@ -9,5 +9,29 @@ let required = true
 console.log(typeof(required))
 
 
-//we cannot redeclare variable with let keyword but possible with var keyword
+//we cannot redeclare variable with let keyword but possible with var keyword */
 
+// *********************LOOPS**********************************
+//if else
+const flag = true
+
+if(!flag)
+{
+console.log("condition statisfied")
+}
+else{
+    console.log("condition not statisfied")   
+}
+
+//while loop
+/* while(true)
+{
+  //  console.log("i am inside loop")
+}*/
+
+let i = 0
+while(i<10)
+{
+    i++
+    console.log(i)
+}
