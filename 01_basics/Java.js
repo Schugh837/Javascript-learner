6e26b596d331feab40bfc713fcf5b6f223bba30a
Schugh837 <87005583+Schugh837@users.noreply.github.com>
2024-04-01 06:59:48 +0000

    Changes done

---
+++ 01_basics/Java.js
@@ -0,0 +1,13 @@
+let a = 4 //let is as a var, if we can declare var use let , const
+console.log(a)
+console.log(typeof(a))
+
+let b = "Silky"
+console.log(typeof(b))
+
+let required = true
+console.log(typeof(required))
+
+
+//we cannot redeclare variable with let keyword but possible with var keyword
+
