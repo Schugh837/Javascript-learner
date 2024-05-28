275bdab2edbe5f2b301cf48a6754a770e2c9b9e6
Schugh837 <87005583+Schugh837@users.noreply.github.com>
2024-05-28 09:33:51 +0000

    Chnages commit

---
+++ 01_basics/classes.js
@@ -0,0 +1,20 @@
+//class is same person 
+
+class Person
+{
+    age = 27
+    get location() //get is used and its treated as property
+    {
+        return "canada"
+    }
+}
+//constructor:-It is method which execute by default when you create object of the class
+
+
+
+
+//how to create object for the class
+let person =new Person()
+console.log(person.age)
+
+console.log(person.location)
